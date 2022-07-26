3c24ad2b9af1e22440d6785fc314d9eee0f55ebe
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-07-26 05:09:52 +0000

    add

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,13 @@
 class CartItem < ApplicationRecord
+  belongs_to :customer
+  belongs_to :item
+
+  validates :customer_id, presence: true
+  validates :item_id, presence: true
+  validates :amount, presence: true
+
+  # カートの小計を求めるメソッド
+  def subtotal
+    item.with_tax_price * amount
+  end
 end

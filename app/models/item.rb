f40b6b496708dbc71e23e6c98e398646c1604991
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-07-26 06:05:50 +0000

    [add]order.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,26 @@
 class Item < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details
+  belongs_to :genre
+
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 0 }, format: { with: /\A[0-9]+\z/ }
+  validates :image_type
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpg')
+    end
+    image.valiant(resize_to_fit: [width, height]).processed
+  end
+
+  # 消費税を求めるメソッド
+  def with_tax_price
+    (price * 1.1).floor
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,15 @@
 class Order < ApplicationRecord
+  has_many :order_details
+  has_many :items, through: :order_details
+  belongs_to :customer
+  
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
+  nalidates :address, presence: true
+  validates :name, presence: true
+  validates :shipping_cost, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :total_payment, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  
+  enum payment_method: {credit_card: 0, transfar: 1 }
+  enum status: {waiting_deposit: 0, confirm_deposit: 1, in_production: 2, preparing_shipment: 3, shipped: 4 }
   
 end

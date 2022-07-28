131258a069b2a8ad704fdc8f095577da8b0ed8a1
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-07-28 08:44:25 +0000

    モデル

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,10 @@
 class OrderDetail < ApplicationRecord
+  belongs_to :item
+  belongs_to :order
+
+  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :amount, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+
+  enum making_status: { cannnot_start: 0, waiting_start: 1, in_production: 2, completed: 3 }
+
 end

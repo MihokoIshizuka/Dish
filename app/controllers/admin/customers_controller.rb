51b4c443557032901d4627aa0bf76782846b97ce
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-02 08:26:27 +0000

    admin/customer/orders

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,17 +1,17 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
     @customers = Customer.all
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
@@ -20,9 +20,14 @@ class Admin::CustomersController < ApplicationController
       render :edit
     end
   end
-  
+
+  def orders
+    @customer = Customer.find(params[:customer_id])
+    @orders = @customer.orders.all
+  end
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
   end

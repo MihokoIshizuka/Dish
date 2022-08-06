4175e076fe4c083f341601bf8a643208030bc87a
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-06 01:47:31 +0000

    admin/order/show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,14 +1,25 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_order_path(@customer.id)
+    end
   end
 
   def customer_order
-    @customer = Customer.find[params(:customer_id)]
+    @customer = Customer.find(params[:customer_id])
     @orders = @customer.Order.all
+  end
+
+  private
+
+  def customer_params
+    params.require(:order).permit(:status)
   end
 
 end

47159c80e9e08d434f28a13f5b70ab2a731bb91a
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-06 01:25:53 +0000

    admin/order/customer_order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,12 +1,14 @@
 class Admin::OrdersController < ApplicationController
-  
+
   def show
   end
-  
+
   def update
   end
-  
+
   def customer_order
+    @customer = Customer.find[params(:customer_id)]
+    @orders = @customer.Order.all
   end
-  
+
 end

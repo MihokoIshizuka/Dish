b410baae33beac458213b2c3003fb3dafb596990
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-08-12 01:18:39 +0000

    seed_fu

--- db/fixtures/production/01_item.rb
+++ db/fixtures/production/01_item.rb
@@ -7,3 +7,33 @@ item = Item.new(
 )
 item.image.attach(io: File.open("./app/assets/images/塩焼きそば.jpg"), filename: '塩焼きそば.jpg', content_type: 'image/jpg')
 item.save!
+
+item = Item.new(
+  genre_id: 1,
+  name: 'お雑煮',
+  introduction: 'お正月。我が家のお雑煮の味。関東風鶏出汁すまし汁タイプのおいしいお雑煮です。',
+  price: 400,
+  is_active: true,
+)
+item.image.attach(io: File.open("./app/assets/images/お雑煮.jpg"), filename: 'お雑煮.jpg', content_type: 'image/jpg')
+item.save!
+
+item = Item.new(
+  genre_id: 3,
+  name: 'アヒージョ',
+  introduction: 'ワインととっても合うアヒージョのセット。スキレットで温めればすぐに完成します。',
+  price: 600,
+  is_active: true,
+)
+item.image.attach(io: File.open("./app/assets/images/アヒージョ.jpg"), filename: 'アヒージョ.jpg', content_type: 'image/jpg')
+item.save!
+
+item = Item.new(
+  genre_id: 3,
+  name: 'ステーキ',
+  introduction: '今日はステーキの気分！そんな時はこちらをどうぞ。すりおろし玉ねぎのステーキソース付きです。。',
+  price: 600,
+  is_active: true,
+)
+item.image.attach(io: File.open("./app/assets/images/ステーキ.jpg"), filename: 'ステーキ.jpg', content_type: 'image/jpg')
+item.save!
